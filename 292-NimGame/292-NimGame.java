caa95ebad90c58cf3e530d7dbbe80f8c66bcdc0c
Poonguzhali0403 <rspoonguzhali@gmail.com>
2026-08-11 14:16:05 +0530

    Create 292-NimGame

---
+++ 292-NimGame/292-NimGame.java
@@ -0,0 +1,11 @@
+// Last updated: 8/11/2026, 2:16:07 PM
+import java.util.*;
+class Solution {
+    public boolean canWinNim(int n) {
+        
+        if(n%4!=0)
+            return true;
+        else
+        return false;
+        }
+    }
