73cf334151aa0a0e8febf39c7a9979f2cb5f8a9c
Poonguzhali0403 <rspoonguzhali@gmail.com>
2026-08-11 14:17:01 +0530

    Create 190-ReverseBits

---
+++ 190-ReverseBits/190-ReverseBits.java
@@ -0,0 +1,15 @@
+// Last updated: 8/11/2026, 2:17:04 PM
+class Solution {
+    public int reverseBits(int n) {
+int result = 0;
+        for (int i = 0; i < 32; i++) {
+            // Extract the last bit of n
+            int bit = n & 1;
+            // Shift result left and add the bit
+            result = (result << 1) | bit;
+            // Shift n right to process the next bit
+            n >>= 1;
+        }
+        return result;
+    }
+    }
