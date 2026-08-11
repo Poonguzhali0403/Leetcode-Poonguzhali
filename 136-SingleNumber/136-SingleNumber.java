68e02b73370b8711dd3c996e41c8618aab3dbea0
Poonguzhali0403 <rspoonguzhali@gmail.com>
2026-08-11 14:17:17 +0530

    Create 136-SingleNumber

---
+++ 136-SingleNumber/136-SingleNumber.java
@@ -0,0 +1,13 @@
+// Last updated: 8/11/2026, 2:17:20 PM
+class Solution {
+    public int singleNumber(int[] nums) {
+        int i,x=0;
+        for(i=0;i<nums.length;i++)
+        
+            x=x^nums[i];
+            return x;
+        
+    }
+        
+    
+}
