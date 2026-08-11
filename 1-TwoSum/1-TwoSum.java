ed6746b041dba5ebfb6e56177b411c231a2946b5
Poonguzhali0403 <rspoonguzhali@gmail.com>
2026-08-11 14:18:20 +0530

    Create 1-TwoSum

---
+++ 1-TwoSum/1-TwoSum.java
@@ -0,0 +1,22 @@
+// Last updated: 8/11/2026, 2:18:23 PM
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int complement = target - nums[i];
+
+            if (map.containsKey(complement)) {
+                return new int[] { map.get(complement), i };
+            }
+
+            map.put(nums[i], i);
+        }
+
+        // Problem guarantees exactly one solution, so this won't be reached.
+        return new int[] {};
+    }
+}
