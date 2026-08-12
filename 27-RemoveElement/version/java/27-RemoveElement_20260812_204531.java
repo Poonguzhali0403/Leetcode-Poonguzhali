7e17c9ef4dc6ab88edce63416e743f5596097888
Poonguzhali0403 <rspoonguzhali@gmail.com>
2026-08-12 20:45:32 +0530

    Create 27-RemoveElement

---
+++ 27-RemoveElement/version/java/27-RemoveElement_20260812_204531.java
@@ -0,0 +1,16 @@
+// Last updated: 8/12/2026, 8:45:31 PM
+1class Solution {
+2    public int removeElement(int[] nums, int val) {
+3        int k = 0; // index for placing non-val elements
+4        
+5        for (int i = 0; i < nums.length; i++) {
+6            if (nums[i] != val) {
+7                nums[k] = nums[i];
+8                k++;
+9            }
+10        }
+11        
+12        return k;
+13    }
+14}
+15
